0b621deee2e858032baec8a10516454317e462e3
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-04-18 15:28:00 +0200

    Update the docker commands to launch

--- build-jenkins.sh
+++ build-jenkins.sh
@@ -10,7 +10,7 @@ docker build -t ubuntu:saucy-oracle-jdk7 - < Dockerfile-oracle-jdk7
 docker build -t ubuntu:saucy-tomcat7 - < Dockerfile-tomcat7
 
 # Now create a container with jenkins based on the tomcat7 images
-docker build -t ubuntu:saucy-jenkins - < Dockerfile-jenkins-in-tomcat7
+docker build -t ubuntu:saucy-jenkins-standalone - < Dockerfile-jenkins-standalone
 
 # Now run the jenkins
-docker run -p 8080:8080 ubuntu:saucy-jenkins
+docker run -p 8080:8080 ubuntu:saucy-jenkins-standalone
